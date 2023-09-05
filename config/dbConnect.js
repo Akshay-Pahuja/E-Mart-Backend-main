e264523a97c5ee59fd708a5c59a478765a97296b
Akshay-Pahuja <21bcs017@iiitdmj.ac.in>
2023-09-05 23:42:21 +0530

    Bckend Complete

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose")
 const dbConnect = ()=>{
  try{
     const conn = mongoose.connect(process.env.MONGODB_URL);
-    console.log("Database connected successfully");
+    console.log(" Database connected successfully ");
  }
  catch(err)
  {
-    console.log("Database error");
+    console.log(" Database error ");
 }
 }
 
